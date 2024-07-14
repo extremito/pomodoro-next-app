c96ecf1d9848059fa1956dc426d151cb1d499694
extr3mito <imarbeal@outlook.com>
2024-07-14 03:00:59 -0700

    Update main  page ui

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,67 +1,76 @@
 'use client'
-import { ChangeEventHandler, useEffect, useState, useRef } from "react";
+import { ChangeEventHandler, useEffect, useState } from "react";
 import TimeDisplay from "@/components/Time-display/TimeDisplay";
-import SimpleTimer from "@/model/Simple-timer/SimpleTimer";
+import Timer from "@/model/Timer/Timer";
 
-const simpleTimer = new SimpleTimer()
+const timer = new Timer()
+
+const cast2Number = (value: unknown) => {
+  switch (typeof value) {
+    case 'number': {
+      return isNaN(value) ? 0 : value
+    }
+    case 'string': {
+      const parsedValue = parseInt(value)
+      return isNaN(parsedValue) ? 0 : parsedValue
+    }
+    default:
+      return 0
+  }
+}
 
 export default function Main() {
 
-  const [time, setTime] = useState(0)
+  const [timeset, setTimeset] = useState('')
   const [timeElapsed, setTimeElapsed] = useState(0)
-  const [timerStarted, setTimerStarted] = useState(false)
-  const swRef = useRef<Worker>()
-  const isTimeLeft = time - timeElapsed > 0
+  const [isCounting, setIsCounting] = useState(timer.isCounting())
 
   const changeTime: ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value
-    setTime(parseInt(value))
-    simpleTimer.setTime(parseInt(value))
+    setTimeset(value)
+    timer.timeset = cast2Number(value)
   }
 
-  const startClick = () => {
-    if (!timerStarted) {
-      simpleTimer.startTimer(() => setTimeElapsed(t => t + 1))
-    } else {
-      simpleTimer.stopTimer()
-    }
-    setTimerStarted(!timerStarted)
-    swRef.current?.postMessage('within start button')
+  const handleStartClick = () => {
+    if (!timer.isCounting())
+      timer.start(setTimeElapsed)
+    setIsCounting(timer.isCounting())
   }
 
-  const resetClick = () => {
+  const handleResetClick = () => {
+    timer.reset()
     setTimeElapsed(0)
-    simpleTimer.stopTimer()
-    setTimerStarted(false)
+    setIsCounting(timer.isCounting())
   }
 
-  useEffect(() => {
-    !isTimeLeft && simpleTimer.stopTimer()
-  }, [isTimeLeft])
-  // Audio
-  // Alert
-  // https://cdn.pixabay.com/audio/2023/06/04/audio_3700fd07a3.mp3
-  // Warning
-  // https://cdn.pixabay.com/audio/2023/10/22/audio_107003231b.mp3
-  useEffect(() => {
-    if(window.Worker) {
-      swRef.current = new Worker(new URL('../../model/Worker-simple-timer/workerSimpleTimer.ts', import.meta.url))
-    }
-    return () => swRef.current?.terminate()
-  }, [])
+  const handlePauseClick = () => {
+    if (timer.isCounting()) timer.pause()
+    setIsCounting(timer.isCounting())
+  }
 
+  // // Audio
+  // // Alert
+  // // https://cdn.pixabay.com/audio/2023/06/04/audio_3700fd07a3.mp3
+  // // Warning
+  // // https://cdn.pixabay.com/audio/2023/10/22/audio_107003231b.mp3
+
+  useEffect(() => {
+    setTimeElapsed(timer.timeElapsed)
+    setIsCounting(timer.isCounting())
+  }, [timeElapsed])
 
   return (<section className="h-full py-4">
-    <div className="w-11/12 bg-red-700 border border-gray-200 rounded-lg shadow flex flex-col items-center m-auto dark:bg-gray-800 dark:border-gray-700">
-      <TimeDisplay timeSet={time} timeElapsed={timeElapsed} />
+    <div className="w-11/12 bg-cyan-600 border border-gray-200 rounded-lg shadow flex flex-col items-center m-auto dark:bg-gray-800 dark:border-gray-700">
+      <TimeDisplay timeSet={cast2Number(timeset)} timeElapsed={timeElapsed} />
       <div className="text-2xl">Set working time</div>
-      <input className="p-1 text-xl dark:bg-slate-600" type="number" onChange={changeTime} value={time} disabled={timerStarted} />
-      <button className={`p-2 bg-lime-700 w-6/12 rounded-lg border-2 border-slate-200 shadow-lg border-double my-1 ${!time ? 'hidden' : ''}`}
-        disabled={!time} onClick={startClick}>{timerStarted ? 'Pause' : 'Start'}</button>
-      <button className={`p-2 bg-slate-500 w-6/12 rounded-lg border-2 border-slate-200 shadow-lg border-double my-1 ${!timeElapsed ? 'hidden' : ''}`}
-        disabled={!timeElapsed} onClick={resetClick}>Reset</button>
-      <div className="text-2xl">{isTimeLeft ? 'Counting' : 'Stopped'}</div>
-      <div className="text-2xl">{isTimeLeft ? 'Ready' : 'Alarm'}</div>
+      <input className="p-1 text-xl dark:bg-slate-600" type="number" onChange={changeTime} value={timeset} disabled={isCounting} placeholder="0" />
+      <button className={`p-2 w-6/12 rounded-lg border-2 bg-lime-700 border-slate-200 shadow-lg border-double my-1 ${!timeset || isCounting || timeElapsed === cast2Number(timeset) ? 'hidden' : ''}`}
+        onClick={handleStartClick}>Start</button>
+      <button className={`p-2 w-6/12 rounded-lg border-2 bg-lime-700 border-slate-200 shadow-lg border-double my-1 ${isCounting ? '' : 'hidden'}`}
+        onClick={handlePauseClick}>Pause</button>
+      <button className={`p-2 bg-yellow-600 w-6/12 rounded-lg border-2 border-slate-200 shadow-lg border-double my-1 ${isCounting || timeElapsed ? '' : 'hidden'}`}
+        onClick={handleResetClick}>Reset</button>
+      {/* <div className="text-2xl">{isTimeLeft ? 'Ready' : 'Alarm'}</div> */}
     </div>
   </section>)
 }
