b7717bb0b12ecf97bee3c1a55e1bba32f88d11fc
extr3mito <imarbeal@outlook.com>
2024-07-14 02:59:59 -0700

    Add constants and types to use globally

---
+++ app/content/constants.ts
@@ -0,0 +1,6 @@
+export const SET_TIME = 'SET_TIME' as const
+export const START_TIMER = 'START_TIMER' as const
+export const START_TIMER_AND_SUBSCRIBE = 'START_TIMER_AND_SUBSCRIBE' as const
+export const STOP_TIMER = 'STOP_TIMER' as const
+export const START_SECONDS = 'START_SECONDS' as const
+export const SECOND_PASSED = 'SECOND_PASSED' as const
---
+++ app/main/types.ts
@@ -0,0 +1,4 @@
+export interface IData {
+  type: string
+  payload: unknown
+}
